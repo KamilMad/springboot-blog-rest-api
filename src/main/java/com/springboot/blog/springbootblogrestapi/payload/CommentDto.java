d6f52642f7d3856852d9e2e9084d57e9bb9a71cf
KamilMadej <kamilul085@gmail.com>
2023-02-06 22:29:17 +0100

    Add PostDto

--- src/main/java/com/springboot/blog/springbootblogrestapi/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/payload/CommentDto.java
@@ -1,4 +1,11 @@
 package com.springboot.blog.springbootblogrestapi.payload;
 
+import lombok.Data;
+
+@Data
 public class CommentDto {
+    private Long id;
+    private String name;
+    private String email;
+    private String content;
 }
