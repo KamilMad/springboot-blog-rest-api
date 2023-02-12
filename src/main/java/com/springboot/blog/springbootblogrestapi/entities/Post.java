5aebba0ecabcee91ad375e9f90cb734aea038353
KamilMadej <kamilul085@gmail.com>
2023-02-12 11:08:22 +0100

    Add GlobalExceptionHandlerClass

--- src/main/java/com/springboot/blog/springbootblogrestapi/entities/Post.java
+++ src/main/java/com/springboot/blog/springbootblogrestapi/entities/Post.java
@@ -1,13 +1,12 @@
 package com.springboot.blog.springbootblogrestapi.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 
